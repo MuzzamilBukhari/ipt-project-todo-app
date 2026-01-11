34a72a3427bc0bfb055654777462dfeca1981981
Muhammad Nabeel Naveed <nabeelnaveed2004@gmail.com>
2026-01-11 03:15:28 -0800

    routing setup done

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div className="bg-amber-900">this is login page</div>
+    <div>This is login component!!</div>
   )
 }
---
+++ frontend/src/pages/signUP/SignUP.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SignUP() {
+  return (
+    <div>This is Sign up component!!</div>
+  )
+}
+
+
