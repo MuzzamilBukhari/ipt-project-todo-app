88e12cadea088e2d9f8682e635c366aeda7202dd
Muhammad Nabeel Naveed <nabeelnaveed2004@gmail.com>
2026-01-11 07:16:14 -0800

    routing set up updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,24 @@
 import Home from './pages/home/home'
 
 
-import { BrowserRouter , Routes, Route, Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUP from './pages/signUP/signUP';
 import Login from './pages/login/login';
-
-
-
-
-
 
 
 function App() {
 
   return (
 
-   
+  <Router>
+
     <Routes>
      <Route path='/dashboard' element={<Home/>} />
      <Route path='/signUp' element={<SignUP/>} />
      <Route path='/login' element={<Login/>} />
     </Routes>
+    
+  </Router>
        
    
    
